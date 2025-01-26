8d4cbaf6a5bb317013489a13597ba251b196356c
chickenbaozi <157216632+chickenbaozi@users.noreply.github.com>
2025-01-26 13:00:26 +0000

    Update main.py
    The code now runs in an infinite loop

--- main.py
+++ main.py
@@ -18,23 +18,23 @@ led.direction = digitalio.Direction.OUTPUT
 
 packetCount = 0
 
-
-led.value = not led.value
-radioMessage = radio.tryRead()
-
-if radioMessage is not None:
-    print("Radio RX {:d} {:s}".format(packet_count, str(radioMessage, "ascii")))
-    print("RSSI: {:3d}db".format(radio.rssi()))
-    packetCount += 1
-
-temp = bmp280.read_temperature(i2c, bmp280_sensor)
-pressure = bmp280.read_pressure(i2c, bmp280_sensor)
-uv = ltr390.read_uv(i2c, ltr390_sensor)
-
-print("\n--------------------------------\n")
-print(f"Cansat Temperature: {temp}")
-print(f"Cansat Pressure:    {pressure}")
-print(f"UV levels:          {uv}")
-
-radio.send(f"------------------\nTeam Raven\n Temperature: {temp}\nPressure:    {pressure}\nUV levels:          {uv}\n------------------")
-print("--- Radio Message sent ---")
+while True:
+    led.value = not led.value
+    radioMessage = radio.tryRead()
+    
+    if radioMessage is not None:
+        print("Radio RX {:d} {:s}".format(packet_count, str(radioMessage, "ascii")))
+        print("RSSI: {:3d}db".format(radio.rssi()))
+        packetCount += 1
+    
+    temp = bmp280.read_temperature(i2c, bmp280_sensor)
+    pressure = bmp280.read_pressure(i2c, bmp280_sensor)
+    uv = ltr390.read_uv(i2c, ltr390_sensor)
+    
+    print("\n--------------------------------\n")
+    print(f"Cansat Temperature: {temp}")
+    print(f"Cansat Pressure:    {pressure}")
+    print(f"UV levels:          {uv}")
+    
+    radio.send(f"------------------\nTeam Raven\n Temperature: {temp}\nPressure:    {pressure}\nUV levels:          {uv}\n------------------")
+    print("--- Radio Message sent ---")
