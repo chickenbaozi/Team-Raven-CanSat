0a7cf1c22ae11ca57809168ed64c55275eac7ae5
chickenbaozi <157216632+chickenbaozi@users.noreply.github.com>
2025-01-26 14:48:37 +0000

    Updated microSD Card code

--- sdCard.py
+++ sdCard.py
@@ -1,8 +1,11 @@
+# https://www.raspberrypi.com/documentation/computers/remote-access.html
 # https://learn.adafruit.com/adafruit-micro-sd-breakout-board-card-tutorial/circuitpython
 import board
 import busio
 import sdcardio
 import storage
+import csv
+from pathlib import Path
 
 # SPI0 TX, SPI0 RX
 # creating spi object - SCK, MOSI, MISO
@@ -15,3 +18,23 @@ vfs = storage.VfsFat(sdcard)
 
 # mount the microSD card's filesystem into the CircuitPython filesystem
 storage.mount(vfs, "/sd")
+
+
+# filename = datetime.datetime.now() + ".csv"
+def cardWrite(filepath, count, temp, pressure, uv, signal):
+    """ Writes to a new csv file everytime the program is restarted """
+    if Path(filepath):
+        exist = True
+    else:
+        exist = False
+    
+    with open(filepath, "w") as file:
+        # creates writer object allowing you to write to csv
+        writer = csv.writer(file)
+        
+        if not exist:
+            writer.writerow(["Packet Count", "RSSI", "Temperature", "Pressure", "UV"])
+            
+        writer.writerow([count, signal, temp, pressure, uv])
+        
+        
