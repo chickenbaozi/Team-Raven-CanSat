da5704a76a3177247b6be2682ae95d2a19ad4a84
chickenbaozi <157216632+chickenbaozi@users.noreply.github.com>
2025-01-28 19:37:46 +0000

    Update sdCard.py
    We can now write to the SD card however code is still not working as intended

--- sdCard.py
+++ sdCard.py
@@ -4,8 +4,8 @@ import board
 import busio
 import sdcardio
 import storage
-import csv
-from pathlib import Path
+import time
+import os
 
 # SPI0 TX, SPI0 RX
 # creating spi object - SCK, MOSI, MISO
@@ -21,20 +21,40 @@ storage.mount(vfs, "/sd")
 
 
 # filename = datetime.datetime.now() + ".csv"
-def cardWrite(filepath, count, temp, pressure, uv, signal):
+def cardWrite(count, temp, pressure, uv, signal):
     """ Writes to a new csv file everytime the program is restarted """
-    if Path(filepath):
-        exist = True
-    else:
-        exist = False
-    
-    with open(filepath, "w") as file:
+    counter = 0
+
+    while True:
+        path = "sd/data" + str(counter) + ".csv"
+        print(path)
+
+        try:
+            with open(path) as tempFile:
+                counter += 1
+
+        except OSError:
+            break
+
+        except:
+            print("ERROR IN CARD WRITE FUNCTION LINE 40")
+
+        #if not os.path.exists(path):
+        #    counter += 1
+        #
+        # else:
+        #     break
+
+    with open(path, "a") as file:
+        time.sleep(0.2)
         # creates writer object allowing you to write to csv
-        writer = csv.writer(file)
-        
-        if not exist:
-            writer.writerow(["Packet Count", "RSSI", "Temperature", "Pressure", "UV"])
-            
-        writer.writerow([count, signal, temp, pressure, uv])
-        
-        
+        # file.write(["Packet Count", "RSSI", "Temperature", "Pressure", "UV"])
+        string = f"{count},{signal},{temp},{pressure},{uv}\n"
+        file.write(string)
+        print("written to", path)
+        time.sleep(0.2)
+
+
+def testWrite(data):
+    with open("/sd/test.txt", "w") as file:
+        file.write("hi world")
